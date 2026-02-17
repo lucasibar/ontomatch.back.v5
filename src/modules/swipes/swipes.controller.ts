c589dcac44d72fa722c2345859e070f1aee9a428
libar <lucas.ibar@gmail.com>
2026-02-16 23:59:37 -0300

    dale antes me hacias vos los comits

---
+++ src/modules/swipes/swipes.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { SwipesService } from './swipes.service';
+import { CreateSwipeDto } from './dto/create-swipe.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Request } from 'express';
+
+@Controller('swipes')
+@UseGuards(JwtAuthGuard)
+export class SwipesController {
+    constructor(private readonly swipesService: SwipesService) { }
+
+    @Post()
+    create(@Body() createSwipeDto: CreateSwipeDto, @Req() req: Request) {
+        const userId = (req.user as any).id;
+        return this.swipesService.create(userId, createSwipeDto);
+    }
+}
--- src/modules/swipes/swipes.module.ts
+++ src/modules/swipes/swipes.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Swipe } from './entities/swipe.entity';
 import { SwipesService } from './swipes.service';
+import { SwipesController } from './swipes.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Swipe])],
+    controllers: [SwipesController],
     providers: [SwipesService],
     exports: [TypeOrmModule, SwipesService],
 })
